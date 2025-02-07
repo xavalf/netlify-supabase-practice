1cbeb661ee611c0972438bef3f8bfb05dca1295d
xavalf <iesvda.xavalf@gmail.com>
2025-02-07 10:05:59 +0100

    Add Supabase integration

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('fetchData').addEventListener('click', async () => {
-    const response = await fetch('https://your-supabase-url.supabase.co/rest/v1/example_table', {
+    const response = await fetch('https://kcoukdhkpxcjrndbcrla.supabase.co', {
         headers: {
-            'apikey': 'your-supabase-api-key',
+            'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtjb3VrZGhrcHhjanJuZGJjcmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MTg2OTQsImV4cCI6MjA1NDQ5NDY5NH0._5oY1SOcPF-olH9o9lQlqbwTQNutStYqXpDzQn6DLjg',
             'Authorization': 'Bearer your-supabase-api-key'
         }
     });
