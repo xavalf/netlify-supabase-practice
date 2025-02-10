1ef63c2c834b6dd666b060d8c5c940c759a244d8
xavalf <iesvda.xavalf@gmail.com>
2025-02-10 10:57:55 +0100

    Add Supabase integration

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 document.getElementById('fetchData').addEventListener('click', async () => {
-    const response = await fetch('https://netlify-supabase-practice.onrender.com');  // Cambia esto por la URL de tu servidor
+    try {
+        const response = await fetch('https://kcoukdhkpxcjrndbcrla.supabase.co/rest/v1/example_table', {
+            headers: {
+                'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtjb3VrZGhrcHhjanJuZGJjcmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MTg2OTQsImV4cCI6MjA1NDQ5NDY5NH0._5oY1SOcPF-olH9o9lQlqbwTQNutStYqXpDzQn6DLjg',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtjb3VrZGhrcHhjanJuZGJjcmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MTg2OTQsImV4cCI6MjA1NDQ5NDY5NH0._5oY1SOcPF-olH9o9lQlqbwTQNutStYqXpDzQn6DLjg',
+                'Content-Type': 'application/json'
+            },
+            mode: "cors"
+        });
 
-    if (response.ok) {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
         const data = await response.json();
         const container = document.getElementById('dataContainer');
         container.innerHTML = JSON.stringify(data, null, 2);
-    } else {
-        console.error('Error al obtener los datos:', response.status, response.statusText);
+    } catch (error) {
+        console.error('Error fetching data:', error);
         const container = document.getElementById('dataContainer');
-        container.innerHTML = `Error: ${response.status} ${response.statusText}`;
+        container.innerHTML = `Error: ${error.message}`;
     }
 });
